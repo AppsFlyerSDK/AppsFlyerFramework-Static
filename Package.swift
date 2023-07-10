04be34141397f038ba77379a99970d986fce6bde
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-07-10 18:28:15 +0300

    Update SPM framework version to v6.12.0

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "AppsFlyerLib-Static",
+    products: [
+        .library(name: "AppsFlyerLib-Static", targets: ["AppsFlyerLib"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AppsFlyerLib",
+            url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework/releases/download/6.12.0/AppsFlyerLib.xcframework.zip",
+            checksum: "b213c8c89ffb1c3dc9f6566c8a9d3fa3d10c57ceedaa720ebc549505f2da96e8"
+        )
+    ]
+)
